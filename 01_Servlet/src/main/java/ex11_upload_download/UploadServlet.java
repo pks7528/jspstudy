703d58bcf8aec35af6c6a5edb7661f60b8090b52
pks7528 <pks7528@naver.com>
2023-03-27 16:58:30 +0900

    2023-03-27

--- 01_Servlet/src/main/java/ex11_upload_download/UploadServlet.java
+++ 01_Servlet/src/main/java/ex11_upload_download/UploadServlet.java
@@ -2,6 +2,10 @@ package ex11_upload_download;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -54,7 +58,28 @@ public class UploadServlet extends HttpServlet {
 		String filesystemName = multipartRequest.getFilesystemName("filename");	// 저장된 첨부 파일명
 		
 		// 첨부 파일 정보
+		// 파일명, 경로, 최종수정일(yyyy-MM-dd), 파일크기(KB)
+		File file = multipartRequest.getFile("filename");
+		String name = file.getName();
+		String parent = file.getParent();
+		String lastModified = new SimpleDateFormat("yyyy-MM-dd").format(file.lastModified());	// 타임스탬프 값 전달
+		String size = new DecimalFormat("#,##0").format(file.length() / 1024 + (file.length() % 1024 == 0 ? 0 : 1));
 		
+		// 응답
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<ul style=\"list-style-type: circle; font-size: 24px;\">");
+		out.println("<li>작성자 : " + uploader + "</li>");
+		out.println("<li>원래 첨부 파일명 : " + originName + "</li>");
+		out.println("<li>저장된 첨부 파일명	: " + filesystemName + "</li>");
+		out.println("<li>파일명 : " + name + "</li>");
+		out.println("<li>경로 : " +  parent + "</li>");
+		out.println("<li>최종수정일 : " + lastModified + "</li>");
+		out.println("<li>파일크기 : " + size + "KB</li>");
+		out.println("<li><a href=\"/01_Servlet/FileListServlet?parent=" + URLEncoder.encode(parent,"UTF-8") + "\">첨부된 파일 목록 보기</a></li>");
+		out.println("</ul>");
+		out.flush();
+		out.close();
 		
 	}
 
