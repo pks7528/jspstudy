ae046c7c52a52fcda011610f883370639822874d
pks7528 <pks7528@naver.com>
2023-03-31 15:26:03 +0900

    2023-03-31

---
+++ 03_Mvc/src/main/java/model/MyService.java
@@ -0,0 +1,9 @@
+package model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface MyService {
+	public String execute(HttpServletRequest request, HttpServletResponse response);
+	
+}
